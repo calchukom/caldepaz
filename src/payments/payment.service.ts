6b725da5f0d43eb5f793f5b640bfa0633f8d2d21
Calchukom <calebogeto1@gmail.com>
2025-08-01 05:06:14 +0300

    added more locations functionalities

--- src/payments/payment.service.ts
+++ src/payments/payment.service.ts
@@ -42,7 +42,7 @@ export class PaymentService {
             throw new Error('STRIPE_SECRET_KEY is required');
         }
 
-        this.stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+        this.stripe = new (Stripe as any)(process.env.STRIPE_SECRET_KEY);
 
         this.mpesaBaseUrl = process.env.MPESA_ENV === 'production'
             ? 'https://api.safaricom.co.ke'
